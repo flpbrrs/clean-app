3a4e89b468955d1857c2fbd560428ec0cb336550
Felipe Barros <felipejonathan.dev@gmail.com>
2024-10-29 19:11:14 -0300

    feat(core): exportanto UsuarioProps

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,8 @@
-import { Usuario } from "./usuario";
+import { Usuario, UsuarioProps } from "./usuario";
 import RepositorioUsuario from "./usuario/providers/RepositorioUsuario";
 import ProvedorCriptografia from "./usuario/providers/ProvedorCriptografia";
 import RegistrarUsuario from "./usuario/services/RegistrarUsuario";
 import LoginUsuario from "./usuario/services/LoginUsuario";
 
-export type { RepositorioUsuario, ProvedorCriptografia }
+export type { RepositorioUsuario, ProvedorCriptografia, UsuarioProps }
 export { Usuario, RegistrarUsuario, LoginUsuario }
--- packages/core/src/usuario/index.ts
+++ packages/core/src/usuario/index.ts
@@ -1,3 +1,5 @@
 import Usuario from "./model/Usuario";
+import { UsuarioProps } from "./model/Usuario";
 
 export { Usuario }
+export type { UsuarioProps }
