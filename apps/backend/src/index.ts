a452127708c7f825c1e770327cc1d9ea5e4fd22f
Felipe Barros <felipejonathan.dev@gmail.com>
2024-10-29 20:47:55 -0300

    feat(backend): registrando a rota de registro de usuários

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,4 +2,13 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-import './external/api/config'
+import app from './external/api/config'
+import RegistrarUsuarioControler from './adapters/RegistrarUsuarioController'
+
+import RepositorioUsuarioPrisma from './external/db/RepositorioUSuarioPrisma'
+import ProvedorCriptografiaBcrypt from './external/auth/ProvedorCriptografiaBcrypt'
+
+const UsuarioRepo = new RepositorioUsuarioPrisma();
+const provCrypto = new ProvedorCriptografiaBcrypt();
+
+new RegistrarUsuarioControler(app, UsuarioRepo, provCrypto);
